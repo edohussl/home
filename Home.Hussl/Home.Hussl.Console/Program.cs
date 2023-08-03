700ee4590c43f1bd138608a4fa4e0ef3fcdf29ff
Edo Hussl <edo.hussl@deftpower.com>
2023-08-03 09:00:58 +0200

    no message

--- Home.Hussl/Home.Hussl.Console/Program.cs
+++ Home.Hussl/Home.Hussl.Console/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Home.Hussl.Console
+﻿using Home.Hussl.Pi;
+
+namespace Home.Hussl.Console
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
 			System.Console.WriteLine("Hello, World!");
+
+			var result = RfWrapper.setupWiringPi();
+
+			if (result == -1)
+			{
+				System.Console.WriteLine("WiringPi setup failed.");
+				return;
+			}
+
+			RfWrapper.sendUnitSignal(3, int.Parse(args[0]), int.Parse(args[1]), bool.Parse(args[2]));
+			System.Console.WriteLine("Command sent.");
 		}
 	}
 }
---
+++ Home.Hussl/Home.Hussl.Pi/RfWrapper.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Home.Hussl.Pi;
+
+public class RfWrapper
+{
+	//public struct NewRemoteCode
+	//{
+	//	public enum SwitchType
+	//	{
+	//		off = 0,
+	//		on = 1,
+	//		dim = 2,
+	//		on_with_dim = 3
+	//	};
+
+	//	public uint period;            // Detected duration in microseconds of 1T in the received signal
+	//	public uint address;          // Address of received code. [0..2^26-1]
+	//	public bool groupBit;                // Group bit set or not
+	//	public SwitchType switchType;          // off, on, dim, on_with_dim.
+	//	public ushort unit;             // Unit code of received code [0..15]
+	//	public ushort dimLevel;         // Dim level [0..15] iff switchType == 2
+	//};
+
+	//public delegate void NewRemoteReceiverCallBack(NewRemoteCode code);
+
+	[DllImport("/home/pi/rpi433/rpi433/RfWrapper.so", EntryPoint = "setupWiringPi")]
+	public static extern int setupWiringPi();
+
+	[DllImport("/home/pi/rpi433/rpi433/RfWrapper.so", EntryPoint = "sendUnitSignal")]
+	public static extern void sendUnitSignal(int pinNumber, int address, int device, bool state);
+
+	//[DllImport("../lib/domotics/raspKaku/libkakuWrapper.so", EntryPoint = "sendGroupSignal")]
+	//public static extern void sendGroupSignal(int pinNumber, int address, bool state);
+
+	//[DllImport("../lib/domotics/raspKaku/libkakuWrapper.so", EntryPoint = "initReceiver")]
+	//public static extern void initReceiver(int pinNumber, NewRemoteReceiverCallBack callback);
+
+	//[DllImport("../lib/domotics/raspKaku/libkakuWrapper.so", EntryPoint = "disableReceiver")]
+	//public static extern void disableReceiver();
+
+	//[DllImport("../lib/domotics/raspKaku/libkakuWrapper.so", EntryPoint = "enableReceiver")]
+	//public static extern void enableReceiver();
+
+	//[DllImport("../lib/domotics/raspKaku/libkakuWrapper.so", EntryPoint = "deinitReceiver")]
+	//public static extern void deinitReceiver();
+}
