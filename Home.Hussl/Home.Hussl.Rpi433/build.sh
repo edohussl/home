093ff3a49a46c35837d66f490862e1f658f13416
Edo Hussl <edo.hussl@deftpower.com>
2023-08-03 08:47:40 +0200

    no message

--- Home.Hussl/Home.Hussl.Rpi433/build.sh
+++ Home.Hussl/Home.Hussl.Rpi433/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 cp * ~/rpi433/rpi433/
-gcc -c -o ~/rpi433/rpi433/RfWrapper.o ~/rpi433/rpi433/RfWrapper.opp
+gcc -c -o ~/rpi433/rpi433/RfWrapper.o ~/rpi433/rpi433/RfWrapper.cpp
 gcc -shared -o ~/rpi433/rpi433/RfWrapper.so ~/rpi433/rpi433/RfWrapper.o
